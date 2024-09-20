9a604939338c9220656f6e2cc1a947a413b941a9
Usuario <Usuario@LAPTOP-TASIUUVD>
2024-09-20 15:35:47 +0200

    cambio3 en conflicto

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -3,7 +3,7 @@ package demo1;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("linea 1");
-		System.out.println("linea 2");
+		System.out.println("Cambio3 mars en conflicto");
 		System.out.println("linea 3");
 		/*Cambio 2 desde mars en rama cambio 2*/
 	}
