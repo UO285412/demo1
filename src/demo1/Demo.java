f7acdd94dd2d3e5aa5d2d884251362e87aa412c9
Usuario <Usuario@LAPTOP-TASIUUVD>
2024-09-20 15:08:32 +0200

    Cambios

--- src/demo1/Demo.java
+++ src/demo1/Demo.java
@@ -1,5 +1,10 @@
 package demo1;
 
 public class Demo {
-
+	public static void main(String[] args) {
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+		System.out.println("linea 1");
+		
+	}
 }
